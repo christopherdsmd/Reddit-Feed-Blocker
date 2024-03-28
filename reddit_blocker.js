12652724c3d15285b07a4aec10f8ea847b0ddb70
Christopher Desmond <chrisdesmond841@gmail.com>
2024-03-28 15:55:26 -0400

    changes

--- reddit_blocker.js
+++ reddit_blocker.js
@@ -1,13 +1,10 @@
 const observer = new MutationObserver(mutations => {
     mutations.forEach(({ target }) => {
-        // Check if the current page is the Reddit homepage or a subreddit page
         if (['/', '/r/all/', '/r/popular/'].includes(window.location.pathname)) {
-            // act only when on the homepage or subreddit pages
-
-            const main = document.querySelector('[role=main]') || document.querySelector('main');
-            if (main) {
-              const parent = main.parentElement;
-              parent.removeChild(main);
+            const mainElement = document.querySelector('[role=main]') || document.querySelector('main');
+            if (mainElement) {
+              const delElement = main.parentElement;
+              delElement.removeChild(main);
             }
         }        
     });
