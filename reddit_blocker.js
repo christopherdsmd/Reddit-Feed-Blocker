e282539a6236cc813896bfe6f8f422b8d5c5d621
Christopher Desmond <chrisdesmond841@gmail.com>
2024-03-28 15:48:51 -0400

    bug fixes

--- reddit_blocker.js
+++ reddit_blocker.js
@@ -1,10 +1,9 @@
 const observer = new MutationObserver(mutations => {
-    console.log("MutationObserver triggered.");
     mutations.forEach(({ target }) => {
         // Check if the current page is the Reddit homepage or a subreddit page
-        if (window.location.pathname === '/' || window.location.pathname.startsWith('/r/')) {
+        if (['/', '/r/all/', '/r/popular/'].includes(window.location.pathname)) {
             // act only when on the homepage or subreddit pages
-            console.log("Targeting node:", target);
+
             const main = document.querySelector('[role=main]') || document.querySelector('main');
             if (main) {
               const parent = main.parentElement;
@@ -19,5 +18,3 @@ observer.observe(document.documentElement, {
     childList: true,
     subtree: true
 });
-
-console.log("Reddit Feed Blocker script loaded.");
